cc24e7481d83f165e7113630a4a64b0776df1143
ecasalin <ecasalin@42.student>
2025-12-18 08:27:52 +0100

    add: ex00 more tests

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,6 +1,8 @@
 #include "easyfind.hpp"
 #include <iostream>
 #include <vector>
+#include <list>
+#include <deque>
 
 int	main(void)
 {
@@ -11,7 +13,29 @@ int	main(void)
 	vec.push_back(4);
 	vec.push_back(5);
 
-	std::cout << (easyfind(vec, 2) == vec.end() ? "Not found" : "Found") << '\n';
-	std::cout << (easyfind(vec, 100) == vec.end() ? "Not found" : "Found") << '\n';
+	std::cout << "VECOR TEST\n" << (easyfind(vec, 2) == vec.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(vec, 100) == vec.end() ? "Not found" : "Found") << "\n\n";
+
+	std::list<int> list;
+	list.push_back(1);
+	list.push_back(2);
+	list.push_back(3);
+	list.push_back(4);
+	list.push_back(5);
+	std::cout << "LIST TEST\n" << (easyfind(list, 5) == list.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(list, 0) == list.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(list, 12) == list.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(list, 6) == list.end() ? "Not found" : "Found") << "\n\n";
+
+	std::deque<int> deque;
+	deque.push_back(1);
+	deque.push_back(2);
+	deque.push_back(3);
+	deque.push_back(4);
+	deque.push_back(5);
+	std::cout << "DEQUE TEST\n" << (easyfind(deque, 112) == deque.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(deque, 99) == deque.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(deque, 4) == deque.end() ? "Not found" : "Found") << '\n'
+			  << (easyfind(deque, 3) == deque.end() ? "Not found" : "Found") << '\n';
 }
 
